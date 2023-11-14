e3c81c83bdb06847a8654132bdfba92d02b7ba91
Srikanth-madasu <142045491+Srikanth-madasu@users.noreply.github.com>
2023-11-14 15:13:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function functionName(){
+	return alert(arguments.callee.name);
+}
+
+function AccioJob(){
+	return alert(arguments.callee.name);
+}
+
+let a = functionName();
+let b = AccioJob();
