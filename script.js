99e7f5f6fe4a0775e36e695a21182b281503bd62
Srikanth-madasu <142045491+Srikanth-madasu@users.noreply.github.com>
2024-03-18 18:20:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 function functionName() {
-  alert(arguments.callee.name);
+ alert(arguments.callee.name);
 }
-function AccioJob() {
-  alert(arguments.callee.name);
-}
-// Example usage
+
 functionName();
-AccioJob()
