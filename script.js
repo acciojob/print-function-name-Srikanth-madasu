5723ac8cc6017220b927f2ffef0a735281dafdeb
Srikanth-madasu <142045491+Srikanth-madasu@users.noreply.github.com>
2024-03-25 22:27:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,2 @@
-//your JS code here. If required.
-function functionName() {
-  alert("functionName");
-}
- 
-// Calling the functionName will display its own name
-functionName();
+
+alert("functionName");
